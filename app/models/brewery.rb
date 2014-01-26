3e09bf75ced1e1a8b0a0036cc8dde76c53b44a45
Olli Koistinen <olko@cs.helsinki.fi>
2014-01-26 22:37:23 +0200

    Average ratings for breweries

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,7 +1,12 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def to_s
     return name
+  end
+
+  def average_rating
+    return self.ratings.average(:score)
   end
 end
