a667781a3ad3dc5a26f9fc6c1affeb51436f843b
Olli Koistinen <olko@cs.helsinki.fi>
2014-01-26 21:10:13 +0200

    ratings view and to_s + beer to_s

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,8 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def to_s
+    return name
+  end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,7 @@
 class Rating < ActiveRecord::Base
   belongs_to :beer
+
+  def to_s
+    return "#{Beer.find_by(id:beer_id)} #{score}"
+  end
 end
