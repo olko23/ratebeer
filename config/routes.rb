23c7f4b2d49676810daadfd8806e836f6e79c12d
Olli Koistinen <olko@cs.helsinki.fi>
2014-02-02 18:28:09 +0200

    signup, signin and signout -links

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Ratebeer::Application.routes.draw do
-  resources :users
-
-  resources :beers
-
-  resources :breweries
-
-  resources :sessions, only: [:new, :create]
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -65,9 +57,20 @@ Ratebeer::Application.routes.draw do
   #get 'ratings', to: 'ratings#index'
   #get 'ratings/new', to: 'ratings#new'
   #post 'ratings', to: 'ratings#create'
+  resources :users
+
+  resources :beers
+
+  resources :breweries
+
+  resources :sessions, only: [:new, :create]
+
   resources :ratings, only: [:index, :new, :create, :destroy]
 
   get 'signup', to: 'users#new'
+
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
   root 'breweries#index'
 end
